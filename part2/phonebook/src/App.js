5fac57999f14d3022dbc5e28b22e74eaa04ee859
Jerry Yang <jerry871002@gmail.com>
2022-10-12 22:20:31 +0300

    part 2b exercises

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNameFilter(event.target.value)
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      setNewName('')
+      setNewNumber('')
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Numberbook</h2>
+      <Filter nameFilter={nameFilter} handleFilterChange={handleFilterChange} />
+      <h2>add a new</h2>
+      <PersonForm 
+        addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
+        newNumber={newNumber} handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
