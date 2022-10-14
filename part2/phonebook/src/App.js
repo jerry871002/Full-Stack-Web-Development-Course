cedfd29286b7e5d104f9ebf22cc3e2834d98b4e7
Jerry Yang <jerry871002@gmail.com>
2022-10-14 13:38:41 +0300

    part 2 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,26 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
+
+import personsService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+  const [notification, setNotification] = useState('')
+  const [notificationType, setNotificationType] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      console.log('axios get response:', response)
-      setPersons(response.data)
-    })
+    personsService
+      .getAll()
+      .then(persons => {
+        setPersons(persons)
+      })
   }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
@@ -28,7 +33,30 @@ const App = () => {
     event.preventDefault()
 
     if (persons.map(person => person.name).includes(newName)) {
-      alert(`${newName} is already added to the phonebook`)
+      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(person => person.name === newName)
+        const updatedPerson = { ...person, number: newNumber }
+        const id = person.id
+        personsService
+          .update(id, updatedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+            setNotification(`Updated ${person.name}`)
+            setNotificationType('success')
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000)
+          })
+          .catch(error => {
+            console.log(error)
+            setPersons(persons.filter(person => person.id !== id))
+            setNotification(`Information of '${person.name}' has already been removed from server`)
+            setNotificationType('error')
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000)
+          })
+      }
       setNewName('')
       setNewNumber('')
       return
@@ -40,14 +68,41 @@ const App = () => {
       id: persons.length + 1
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    personsService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+        setNotification(`Added ${newName}`)
+        setNotificationType('success')
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      })
+  }
+
+  const deletePerson = id => {
+    return function(e) {
+      e.preventDefault()
+      if (window.confirm(`Delete ${persons.find(p => p.id === id).name}?`)) {
+        personsService
+          .remove(id)
+          .then(data => {
+            personsService
+              .getAll()
+              .then(persons => {
+                setPersons(persons)
+              })
+          })
+      }
+    }
   }
 
   return (
     <div>
       <h2>Numberbook</h2>
+      <Notification message={notification} type={notificationType} />
       <Filter nameFilter={nameFilter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm 
@@ -55,7 +110,7 @@ const App = () => {
         newNumber={newNumber} handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} deleteHandler={deletePerson} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,4 +1,4 @@
-const Persons = ({ persons }) => 
-    persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
+const Persons = ({ persons, deleteHandler }) => 
+  persons.map(person => <p key={person.id}>{person.name} {person.number} <button onClick={deleteHandler(person.id)}>delete</button></p>)
 
 export default Persons
