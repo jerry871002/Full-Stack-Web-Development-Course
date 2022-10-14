035e0c9628401b202a374ed6581fb9645c1657d3
Jerry Yang <jerry871002@gmail.com>
2022-10-14 10:53:25 +0300

    countries app part 2 (exercise 2.14)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,30 +1,70 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Countries = ({ countries }) => {
-  if (countries.length === 0) {
-    return <div>No match</div>
-  } else if (countries.length >= 10) {
-    return <div>Too many matches, specify another filter</div>
-  } else if (countries.length === 1) {
-    const country = countries[0]
-    return (
-      <div>
-        <h2>{country.name} {country.flag}</h2>
-        <span>Capital: {country.capital}</span><br/>
-        <span>Area: {country.area}</span>
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-        </ul>
-      </div>
-    )
-  } else {
-    return (
+const SingleCountry = ({ country }) => {
+  const [ weatherData, setWeatherData ] = useState({})
+
+  const api_key = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo[0]}&lon=${country.capitalInfo[1]}&units=metric&appid=${api_key}`)
+      .then(response => {
+        console.log('axios get weather response:', response)
+        setWeatherData(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h2>{country.name} {country.flag}</h2>
+      <span>Capital: {country.capital}</span><br/>
+      <span>Area: {country.area}</span>
+      <h3>Languages:</h3>
       <ul>
-        {countries.map(country => <li key={country.name}>{country.name}</li>)}
+        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
       </ul>
-    )
+      <h2>Weather in {country.capital}</h2>
+      <span>Temperature: {(function() {
+        if (Object.keys(weatherData).length !== 0) 
+          return weatherData.main.temp
+        })()
+      } Celcius</span><br/>
+      <span>Wind: {(function() {
+        if (Object.keys(weatherData).length !== 0) 
+          return weatherData.wind.speed
+        })()
+      } m/s</span>
+    </div>
+  )
+}
+
+const Countries = ({ countries, setCountryFilter }) => {
+  const showCountry = name => {
+    return function(e) {
+      e.preventDefault()
+      setCountryFilter(name)
+    }
+  }
+
+  return (
+    <ul>
+      {countries.map(country => 
+        <li key={country.name}>{country.name} <button onClick={showCountry(country.name)}>show</button></li>
+      )}
+    </ul>
+  )
+}
+
+function showCountries(countriesToShow, countryFilter, setCountryFilter) {
+  if (countriesToShow.length === 0 || countryFilter === '') {
+    return <div>No match</div>
+  } else if (countriesToShow.length >= 10) {
+    return <div>Too many matches, specify another filter</div>
+  } else if (countriesToShow.length === 1) {
+    return <SingleCountry country={countriesToShow[0]} />
+  } else {
+    return <Countries countries={countriesToShow} setCountryFilter={setCountryFilter} />
   }
 }
 
@@ -40,6 +80,7 @@ function App() {
             name: country.name.common,
             area: country.area,
             capital: country.capital,
+            capitalInfo: country.capitalInfo.latlng,
             languages: country.languages,
             flag: country.flag
           }
@@ -59,10 +100,7 @@ function App() {
         value={countryFilter}
         onChange={handleFilterChange}
       />
-      <Countries countries={(function() {
-        if (countryFilter !== '') return countriesToShow
-        return []
-      })()} />
+      {showCountries(countriesToShow, countryFilter, setCountryFilter)}
     </div>
   )
 }
