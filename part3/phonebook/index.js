9fe09fdb09797025cfd2b1ec460e048fa94543dd
Jerry Yang <jerry871002@gmail.com>
2022-12-10 14:18:47 +0200

    part 3 backend done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -15,19 +15,6 @@ app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :body'
 ))
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-// this has to be the last loaded middleware
-app.use(errorHandler)
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -45,19 +32,12 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!(body.name && body.number)) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const names = persons.map(person => person.name)
-  if (names.includes(body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -66,9 +46,11 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -77,13 +59,13 @@ app.get('/api/persons/:id', (request, response, next) => {
       if (person) {
         response.json(person)
       } else {
-        response.status(404).end() 
+        response.status(404).end()
       }
     })
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
@@ -112,6 +94,21 @@ const unknownEndpoint = (request, response) => {
 
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || '8080'
 app.listen(PORT, () => {
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -13,8 +13,45 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(number) {
+        if (number.length < 8) {
+          return false
+        }
+
+        const splitedNumber = number.split('-')
+        if (splitedNumber.length > 2) {
+          return false
+        }
+
+        // if formed of two parts that are separated by "-"
+        // the first part should have two or three numbers
+        if (
+          splitedNumber.length === 2 &&
+          (splitedNumber[0].length < 2 || splitedNumber[0].length > 3)
+        ) {
+          return false
+        }
+
+        // all elements should be numbers
+        splitedNumber.forEach(element => {
+          if (isNaN(element)) {
+            return false
+          }
+        })
+
+        return true
+      }
+    }
+  },
 })
 
 personSchema.set('toJSON', {
